759ec4dbae64ef9533f6c168d7b9dac91980f576
fiskpappa <h23felno@du.se>
2024-11-27 12:40:03 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementsByClassName("checkbox");
+let boxen = "checkbox";
+
+document.querySelectorAll(".textfield");
+let textfält = ".textfield";
+
+document.getElementById("#button");
+let knapp = "#button";
+
+document.querySelector("#ruta");
+let ruta = "#ruta";
+
+function inputEffect() {
+    
+}
